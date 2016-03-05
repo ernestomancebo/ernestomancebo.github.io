5282c4d9aafb2ef6e96179f2d05dcd9c15471184
Ernesto Manebo <ernesmancebo@gmail.com>
2016-03-04 21:15:31 -0400

    fixing typos

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,8 +21,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "http://mail-master.herokuapp.com:80/mail/ernesto",
+                method: "POST",
+                // type: "POST",
+                crossDomain: true,
+                dataType: 'json',
                 data: {
                     name: name,
                     phone: phone,
@@ -31,40 +34,41 @@ $(function() {
                 },
                 cache: false,
                 success: function() {
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                                    // Enable button & show success message
+                                    $("#btnSubmit").attr("disabled", false);
+                                    $('#success').html("<div class='alert alert-success'>");
+                                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                    .append("</button>");
+                                    $('#success > .alert-success')
+                                    .append("<strong>Your message has been sent. </strong>");
+                                    $('#success > .alert-success')
+                                    .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+                                    //clear all fields
+                                    $('#contactForm').trigger("reset");
+                                },
+                                error: function() {
+                                    // Fail message
+                                    $('#success').html("<div class='alert alert-danger'>");
+                                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                                    .append("</button>");
+                                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                                    $('#success > .alert-danger').append('</div>');
+                                    //clear all fields
+                                    $('#contactForm').trigger("reset");
+                                }
 
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
+                            })
+},
+filter: function() {
+    return $(this).is(":visible");
+},
+});
+
+$("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+});
 });
 
 // When clicking on Full hide fail/success boxes
